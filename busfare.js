b999bd8c073e8d89e29e63dd12d36055f01523a7
adnanahid <sikuahamed@gmail.com>
2024-08-30 19:51:43 +0600

    error

---
+++ busfare.js
@@ -0,0 +1,14 @@
+let age = 22;
+let id = "student";
+if (age<10) {
+    console.log("free")
+}
+else if ("student") {
+    console.log("50% off")
+}
+else if (age>=60) {
+    console.log("15% discount")
+}
+else {
+    console.log("800 taka")
+}
