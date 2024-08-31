bd923375605ea829b81e1a04f977509c9d56a0e8
adnanahid <sikuahamed@gmail.com>
2024-08-31 20:31:15 +0600

    ok

--- grads.js
+++ grads.js
@@ -1,23 +1,18 @@
 const mark = 48;
 
-if ( mark >= 80 && mark <=100 ) {
-    console.log("A+");
+if (mark >= 80 && mark <= 100) {
+  console.log("A+");
+} else if (mark >= 70) {
+  console.log("A");
+} else if (mark >= 60) {
+  console.log("A-");
+} else if (mark >= 50) {
+  console.log("B");
+} else if (mark >= 40) {
+  console.log("C");
+} else if (mark >= 33) {
+  console.log("D");
+} else {
+  console.log("Fail");
 }
-else if ( mark >= 70 ) {
-    console.log("A");
-}
-else if ( mark >= 60 ) {
-    console.log("A-");
-}
-else if ( mark >= 50 ) {
-    console.log("B");
-}
-else if ( mark >= 40 ) {
-    console.log("C");
-}
-else if ( mark >= 33 ) {
-    console.log("D");
-}
-else {
-    console.log("Fail");
-}
+// ! todo nav section star here
--- ternary.js
+++ ternary.js
@@ -1,1 +1,15 @@
-num1 =
+// num1 = 10;
+// num2 = 30;
+// result = num1*2;
+
+// if (num1 > num2) {
+//     console.log(result)
+// }
+// else {
+//     console.log(num1+num2)
+// }
+num1 = 10; 
+num2 = 20; 
+result=num1*2;
+ 
+num1>num2 ? result :  num1 + num2;
