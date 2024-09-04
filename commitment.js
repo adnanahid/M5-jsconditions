33cd82908c163f3fdd0e7082968cc8390b571ed1
adnanahid <sikuahamed@gmail.com>
2024-09-04 19:56:54 +0600

    commitment done

---
+++ commitment.js
@@ -0,0 +1,8 @@
+// let num =0;
+// while(num <= 60){
+//     num++;
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!"+num)
+// }
+for(num =0; num <= 60; num++){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!"+num)
+}
---
+++ for.js
@@ -0,0 +1,5 @@
+let sum = 0;
+for (let num = 11; num < 21; num += 1){
+    sum = sum+num;
+}
+console.log(sum)
