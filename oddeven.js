8f305f9c0b39625761d0956e7ef8e154a861eb43
adnanahid <sikuahamed@gmail.com>
2024-09-04 21:19:28 +0600

    practice

---
+++ oddeven.js
@@ -0,0 +1,72 @@
+// ! Find all the odd numbers from 61 to 100.
+// let num = 61;
+
+// while (num <= 100) {
+//   if (num % 2 == 1) {
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+// ! Find all the even numbers from 78 to 98.
+// for (num = 78; num <= 98; num++) {
+//   if (num % 2 == 0) {
+//     console.log(num);
+//   }
+// }
+
+//! Display sum of all the odd numbers from 81 to 131.
+// let num = 81;
+// let sum = 0;
+// while (num <= 131) {
+//     sum += num;
+//     if (num % 2 == 1) {
+//   }
+//   num++;
+// }
+// console.log(sum);
+
+//!Display sum of all the even numbers from 206 to 311.
+// let sum=0;
+// for (let num = 206; num <= 311; num++) {
+//   if (num % 2 == 1) {
+//     sum+=num;
+//   }
+// }
+// console.log(sum)
+
+//! generate a multiplication table for number 5
+// let number = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(number + " x " + i + " = " + (number * i));
+// }
+
+//!Implement a countdown timer that counts down from 21 to 15.
+// let count = 21;
+// let countdown = setInterval(function() {
+//   console.log(count);
+//   count--;
+
+//   if (count < 15) {
+//     clearInterval(countdown);
+//   }
+// }, 1000);
+
+//!Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers
+// for(num = 1; num<=40; num++){
+//     if (num % 2 !== 0){
+//         continue
+//     }
+// console.log(num)
+// }
+
+//! display odd number from 55 to 85 and skip the numbers divisible by 5.
+// for(num = 55; num<=85; num++){
+//     if (num % 5 == 0){
+//         continue
+//     }
+// console.log(num)
+// }
+
+
